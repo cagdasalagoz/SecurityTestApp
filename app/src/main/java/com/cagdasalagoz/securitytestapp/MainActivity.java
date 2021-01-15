36a0cd31c4597e0298549762d0012318e825a176
Çağdaş Alagöz <cagdasalagoz@gmail.com>
2021-01-15 22:42:21 +0300

    update logs

--- app/src/main/java/com/cagdasalagoz/securitytestapp/MainActivity.java
+++ app/src/main/java/com/cagdasalagoz/securitytestapp/MainActivity.java
@@ -1,12 +1,10 @@
 package com.cagdasalagoz.securitytestapp;
 
-import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -19,8 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.e("CAGDAS", "device model: " + android.os.Build.MODEL);
-        Log.e("CAGDAS", "device sdk version: " + android.os.Build.VERSION.SDK);
+        Log.e("CAGDAS", "sdk version: " + android.os.Build.VERSION.SDK);
         Log.e("CAGDAS", "device: " + android.os.Build.DEVICE);
         Log.e("CAGDAS", "model: " + android.os.Build.MODEL);
         Log.e("CAGDAS", "product: " + android.os.Build.PRODUCT);
@@ -30,8 +27,10 @@ public class MainActivity extends AppCompatActivity {
                 .getInstalledApplications(PackageManager.GET_META_DATA);
         Log.e("CAGDAS", "packages: " + packages.toString());
 
-        TextView tvName = findViewById(R.id.textField);
-        tvName.setText(packages.toString());
+        TextView textField = findViewById(R.id.textField);
+        textField.setText(packages.toString());
+        TextView appCount = findViewById(R.id.AppCount);
+        appCount.setText("Installed app count:" + packages.size());
     }
 
 }
